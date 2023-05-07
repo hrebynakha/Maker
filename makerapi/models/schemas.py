0c4e939c97a7b5f6795d4f053ba2b5eb8ab14499
Hrebynakha Anatolii <hrebynakha@gmail.com>
2023-05-07 19:35:20 +0300

    remove db

--- makerapi/models/schemas.py
+++ makerapi/models/schemas.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-DATABASE_URL = "sqlite:///./cooking.db"  # Replace with your database URL
+DATABASE_URL = "sqlite:///./sqlite.db"  # Replace with your database URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
